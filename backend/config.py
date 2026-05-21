810a283f28ab3b99a0840430ef54dfaa1e608fbd
chansy152 <chandanalokhande152@gmail.com>
2026-05-21 11:36:00 +0530

    Fixed requirements structure

--- backend/config.py
+++ backend/config.py
@@ -1,12 +1,11 @@
 import os
 
-class Config:
-    db_url = os.environ.get("DATABASE_URL")
+db_url = os.getenv("DATABASE_URL")
 
-    if db_url:
-        db_url = db_url.replace("postgres://", "postgresql://", 1)
+if db_url.startswith("postgres://"):
+    db_url = db_url.replace("postgres://", "postgresql://", 1)
 
-    SQLALCHEMY_DATABASE_URI = db_url
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
+if "sslmode" not in db_url:
+    db_url += "?sslmode=require"
 
-    JWT_SECRET_KEY = os.environ.get("JWT_SECRET_KEY")
+app.config["SQLALCHEMY_DATABASE_URI"] = db_url

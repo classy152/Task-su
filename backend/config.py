d61ae337ceaaa8948f55eb72cc6528ceb92b4ba7
chansy152 <chandanalokhande152@gmail.com>
2026-05-21 14:37:05 +0530

    Fixed requirements structure

--- backend/config.py
+++ backend/config.py
@@ -8,6 +8,13 @@ class Config:
         db_url = db_url.replace("postgres://", "postgresql://", 1)
 
     SQLALCHEMY_DATABASE_URI = db_url
+
+    SQLALCHEMY_ENGINE_OPTIONS = {
+        "connect_args": {
+            "sslmode": "disable"
+        }
+    }
+
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
     JWT_SECRET_KEY = os.environ.get("JWT_SECRET_KEY")

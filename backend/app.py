44c738115c77f9427e85d2da248231a7104ef3de
chansy152 <chandanalokhande152@gmail.com>
2026-05-21 10:43:57 +0530

    Fix database config

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,12 @@
 from flask import Flask
+import os
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route("/")
 def home():
     return "Backend Running Successfully"
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host="0.0.0.0", port=port)

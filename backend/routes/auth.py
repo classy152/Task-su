adbf0446be8f162280e10c6742eba7ec3981f787
chansy152 <chandanalokhande152@gmail.com>
2026-05-21 09:23:49 +0530

    Fixed absolute backend package imports

--- backend/routes/auth.py
+++ backend/routes/auth.py
@@ -2,7 +2,7 @@ from flask import Blueprint, request, jsonify
 from flask_bcrypt import Bcrypt
 from flask_jwt_extended import create_access_token
 
-from model import db, User
+from backend.model import db, User
 
 auth_bp = Blueprint('auth', __name__)
 
